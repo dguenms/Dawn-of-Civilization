d706e6792c26bf37ea6c201c89555a0b68b32f34
Leoreth <Leoreth@94cdc8ef-5f1a-49b8-8b82-819f55b6d80d>
2011-08-26 10:09:53 +0000

    Adjustments for the Indonesian UHV.

--- Assets/Python/Victory.py
+++ Assets/Python/Victory.py
@@ -1083,7 +1083,7 @@ class Victory:
 					else:
 						popPercent = 0.0
 
-					if popPercent >= 20.0:
+					if popPercent >= 10.0:
 						self.setGoal(iIndonesia, 2, 1)
 					else:
 						self.setGoal(iIndonesia, 2, 0)
@@ -2688,7 +2688,7 @@ class Victory:
 					popPercent = (ourPop * 100.0) / totalPop
 				else:
 					popPercent = 0.0
-				aHelp.append(self.getIcon(popPercent >= 20.0) + 'Percentage of world population: ' + str(popPercent) + '/20 %')
+				aHelp.append(self.getIcon(popPercent >= 10.0) + 'Percentage of world population: ' + (u"%.2f%%" % popPercent) + '/10%')
 
 		elif iPlayer == iSpain:
 			if iGoal == 1:
