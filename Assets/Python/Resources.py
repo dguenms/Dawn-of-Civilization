35604cc2d34b53533d970843e80b037340a714ed
leoreth <leoreth@94cdc8ef-5f1a-49b8-8b82-819f55b6d80d>
2014-09-11 15:19:01 +0000

    v1.12 Release.

--- Assets/Python/Resources.py
+++ Assets/Python/Resources.py
@@ -65,7 +65,7 @@ class Resources:
 				bWater = gc.getMap().plot(iX, iY).isWater()
 				city = gc.getMap().findCity(iX, iY, iOwner, TeamTypes.NO_TEAM, not bWater, bWater, TeamTypes.NO_TEAM, DirectionTypes.NO_DIRECTION, CyCity())
 				if not city.isNone():
-					szText = localText.getText(textKey, (gc.getBonusInfo(iBonus).getTextKey(), city.getName(), gc.getPlayer(iOwner).getCivilizationAdjective(0)))
+					szText = localText.getText(textKey, (gc.getBonusInfo(iBonus).getTextKey(), city.getName()))
 					CyInterface().addMessage(iOwner, False, con.iDuration, szText, "AS2D_DISCOVERBONUS", InterfaceMessageTypes.MESSAGE_TYPE_MINOR_EVENT, gc.getBonusInfo(iBonus).getButton(), ColorTypes(con.iWhite), iX, iY, True, True)
 
 
