f59a636163259d5528848f8eba330ad961a15a66
Bautos42 <tobiasose@web.de>
2016-04-11 15:11:57 +0200

    Added Castilla, William III and George I into the game and removed the eagle from the cape of Casimir III.

---
+++ Assets/Art/leaderheads/George I/peter_bg_anim.h
@@ -0,0 +1,16 @@
+// This file was automatically generated. It contains definitions for all the
+// animations stored in the associated KFM file. Include this file in your
+// final application to easily refer to animation sequences.
+
+#ifndef PETER_BG_ANIM_H__
+#define PETER_BG_ANIM_H__
+
+namespace peter_BG_Anim
+{
+    enum
+    {
+        BACKGROUND              = 0
+    };
+}
+
+#endif  // #ifndef PETER_BG_ANIM_H__
