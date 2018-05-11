720a6ea120b0544fe0edcb8c060e80788b53ee1d
dguenms <dguenms@gmail.com>
2018-05-11 19:11:48 +0200

    Restore DLL coded wonder effects

--- CvGameCoreDLL/CvRhyes.h
+++ CvGameCoreDLL/CvRhyes.h
@@ -11,7 +11,7 @@ typedef list<char*> LISTCHAR;
 
 #define MAX_COM_SHRINE			(20)
 
-#define BEGIN_WONDERS				(162) // increment if normal building (not for wonders) is added
+#define BEGIN_WONDERS				(163) // increment if normal building (not for wonders) is added
 #define BEGIN_GREAT_WONDERS			(BEGIN_WONDERS+12)
 #define NUM_BUILDINGS_PLAGUE		(237) // always increment when a building is added
 #define NUM_BUILDINGTYPES_PLAGUE	(158) // increment when a building class is added
