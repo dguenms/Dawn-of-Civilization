1cdfa06c2f5e05c19fffbb7f3eedaee283715ef3
dguenms <dguenms@gmail.com>
2018-06-11 02:10:03 +0200

    Add more wonders
    - Cheomseongdae
    - House of Wisdom
    - Krak des Chevaliers
    - Nalanda University
    - Saint Sophia's Cathedral
    - Salsal Buddha
    - Santa Maria del Fiore

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -872,7 +872,7 @@ iSugar, iTea, iTobacco, iWine, iWhales, iSoccer, iSongs, iMovies) = range(iNumBo
 
 # Buildings
 
-iNumBuildings = 241
+iNumBuildings = 248
 (iPalace, iWalls, iDun, iCastle, iCitadel, iStarFort, iBarracks, iIkhanda, iStable, iGer, 
 iBunker, iBombShelters, iGranary, iTerrace, iSmokehouse, iAqueduct, iBaray, iNoria, iStepwell, iBath, 
 iReservoir, iHammam, iPharmacy, iAlchemist, iSewer, iPark, iHospital, iRecyclingCenter, iLighthouse, iTradingPost, 
@@ -893,12 +893,19 @@ iArmoury, iAdministrativeCenter, iSecretService, iTriumphalArch, iNationalTheatr
 iNationalMonument, iIronworks, iMilitaryAcademy, iNationalPark, iRedCross, iGreatSphinx, iGreatLighthouse, iGreatCothon, iTerracottaArmy, iTempleOfArtemis, 
 iPyramids, iHangingGardens, iOracle, iMoaiStatues, iIshtarGate, iApadanaPalace, iColossus, iParthenon, iStatueOfZeus, iShwedagonPaya, 
 iKhajuraho, 
-iGreatLibrary, iMausoleumOfMaussollos, iGondeshapur, iFloatingGardens, iColosseum, iGreatWall, iJetavanamaraya, iTheodosianWalls, iMachuPicchu, iPrambanan,
+iGreatLibrary, iMausoleumOfMaussollos, iGondeshapur, iFloatingGardens, iColosseum, iGreatWall, iJetavanamaraya, iTheodosianWalls, iMachuPicchu, iSalsalBuddha,
+iPrambanan,
 iBorobudur, 
 iGrandCanal, 
 iHagiaSophia, 
-iNotreDame, iTempleOfKukulkan, iHimejiCastle, iBlueMosque, iWatPreahPisnulok, iTopkapiPalace, iMezquita, iSistineChapel, iLeaningTower, iRedFort, 
-iVersailles, iForbiddenPalace, iSpiralMinaret, iDomeOfTheRock, iUniversityOfSankore, iTajMahal, iSanMarcoBasilica, iPorcelainTower, iSaintBasilsCathedral, iHarmandirSahib, 
+iNotreDame, iSaintSophia, iTempleOfKukulkan, iHimejiCastle, iKrakDesChevaliers, iBlueMosque, iWatPreahPisnulok, iTopkapiPalace, iMezquita, iCheomseongdae, 
+iNalanda, 
+iSistineChapel, 
+iLeaningTower, 
+iSantaMariaDelFiore,
+iRedFort, 
+iVersailles, iForbiddenPalace, iSpiralMinaret, iDomeOfTheRock, iHouseOfWisdom, iUniversityOfSankore, iTajMahal, iSanMarcoBasilica, iPorcelainTower, iSaintBasilsCathedral, 
+iHarmandirSahib, 
 iTrafalgarSquare, iBrandenburgGate, iStatueOfLiberty, iPentagon, iLubyanka, iWestminsterPalace, iEiffelTower, iEmpireStateBuilding, iCERN, iWembley, 
 iGraceland, iCristoRedentor, iItaipuDam, iHollywood, iUnitedNations, iCNTower, iSpaceElevator) = range(iNumBuildings)
 
