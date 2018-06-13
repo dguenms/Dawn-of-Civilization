0e19e0b1e790898d969d33f5f139c9787d76bd52
dguenms <dguenms@gmail.com>
2018-06-14 01:08:28 +0200

    Add more wonders
    - Hermitage
    - Bell Rock Lighthouse
    - Emerald Buddha Temple
    - The Louvre
    - Salt Cathedral

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -872,7 +872,7 @@ iSugar, iTea, iTobacco, iWine, iWhales, iSoccer, iSongs, iMovies) = range(iNumBo
 
 # Buildings
 
-iNumBuildings = 256
+iNumBuildings = 261
 (iPalace, iWalls, iDun, iCastle, iCitadel, iStarFort, iBarracks, iIkhanda, iStable, iGer, 
 iBunker, iBombShelters, iGranary, iTerrace, iSmokehouse, iAqueduct, iBaray, iNoria, iStepwell, iBath, 
 iReservoir, iHammam, iPharmacy, iAlchemist, iSewer, iPark, iHospital, iRecyclingCenter, iLighthouse, iTradingPost, 
@@ -909,7 +909,11 @@ iRedFort,
 iVersailles, iForbiddenPalace, iSpiralMinaret, iDomeOfTheRock, iHouseOfWisdom, iUniversityOfSankore, iTajMahal, iSanMarcoBasilica, iPorcelainTower, iSaintBasilsCathedral, 
 iEscorial,
 iHarmandirSahib,
-iTrafalgarSquare, iPotalaPalace, iShalimarGardens, iImageOfTheWorldSquare, iBourse, iItsukushimaShrine, iBrandenburgGate, iStatueOfLiberty, iPentagon, iLubyanka, 
+iTrafalgarSquare, iPotalaPalace, iShalimarGardens, iImageOfTheWorldSquare, iBourse, iItsukushimaShrine, iLouvre, iHermitage, iSaltCathedral, iEmeraldBuddha,
+iBellRockLighthouse,
+iBrandenburgGate, 
+iStatueOfLiberty, iPentagon, 
+iLubyanka, 
 iWestminsterPalace, 
 iEiffelTower, 
 iEmpireStateBuilding, 
