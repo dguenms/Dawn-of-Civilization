b13829908401db84395cf6c141f36421edf8e757
dguenms <dguenms@gmail.com>
2018-07-01 15:26:17 +0200

    Change Triumphal Arch from Nation Wonder into Great Wonder

--- Assets/Python/Victory.py
+++ Assets/Python/Victory.py
@@ -1497,8 +1497,7 @@ def checkHistoricalVictory(iPlayer):
 		if countAchievedGoals(iPlayer) >= 2:	
 			data.players[iPlayer].bHistoricalGoldenAge = True
 			
-			capital = pPlayer.getCapitalCity()
-			capital.setHasRealBuilding(iTriumphalArch, True)
+			gc.getPlayer(iPlayer).changeGoldenAgeTurns(gc.getPlayer(iPlayer).getGoldenAgeTurns())
 			
 			if pPlayer.isHuman():
 				CyInterface().addMessage(iPlayer, False, iDuration, CyTranslator().getText("TXT_KEY_VICTORY_INTERMEDIATE", ()), "", 0, "", ColorTypes(iPurple), -1, -1, True, True)
