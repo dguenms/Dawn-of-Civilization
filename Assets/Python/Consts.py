ccc409c05de5f5ba5f43d7d162b9525d60699c08
dguenms <dguenms@gmail.com>
2018-07-29 22:24:35 +0200

    Add Metropolitain wonder

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -872,7 +872,7 @@ iSugar, iTea, iTobacco, iWine, iWhales, iSoccer, iSongs, iMovies) = range(iNumBo
 
 # Buildings
 
-iNumBuildings = 298
+iNumBuildings = 299
 (iPalace, iWalls, iDun, iCastle, iCitadel, iStarFort, iBarracks, iIkhanda, iStable, iGer, 
 iBunker, iBombShelters, iGranary, iTerrace, iSmokehouse, iAqueduct, iBaray, iNoria, iStepwell, iBath, 
 iReservoir, iHammam, iPharmacy, iAlchemist, iSewer, iPark, iHospital, iRecyclingCenter, iLighthouse, iTradingPost, 
@@ -896,16 +896,13 @@ iKhajuraho, iGreatLibrary, iMausoleumOfMaussollos, iAlKhazneh, iGondeshapur, iFl
 iGreatWall, iJetavanaramaya, iTheodosianWalls, iMachuPicchu, iSalsalBuddha, iPrambanan, iBorobudur, iMonolithicChurch, iGrandCanal, iHagiaSophia, 
 iNotreDame, iSaintSophia, iTempleOfKukulkan, iHimejiCastle, iAlamut, iKrakDesChevaliers, iSaintThomasChurch, iBlueMosque, iWatPreahPisnulok, iVijayaStambha, 
 iTopkapiPalace, iSilverTreeFountain, iMezquita, iCheomseongdae, iNalanda, iSistineChapel, iSantaMariaDelFiore, iGurEAmir, iRedFort, iVersailles, 
-iForbiddenPalace, 
-iSpiralMinaret, iDomeOfTheRock, iHouseOfWisdom, iUniversityOfSankore, iTajMahal, iOxfordUniversity, iSanMarcoBasilica, iPorcelainTower, iSaintBasilsCathedral, iEscorial,
-iTorreDeBelem, iHarmandirSahib, iTrafalgarSquare, iPotalaPalace, iShalimarGardens, iImageOfTheWorldSquare, iBourse, iItsukushimaShrine, iLouvre, iHermitage, 
-iSaltCathedral, 
-iAmberRoom, iEmeraldBuddha, iGuadalupeBasilica, iBellRockLighthouse, iBrandenburgGate, iStatueOfLiberty, iTriumphalArch, iMoleAntonelliana, iNeuschwanstein, iBrooklynBridge, 
-iCrystalPalace, iTsukijiFishMarket, iPentagon, iMotherlandCalls, iLubyanka, iWestminsterPalace, iEiffelTower, iEmpireStateBuilding, iPalaceOfNations, iSagradaFamilia, 
-iBerlaymont,
-iCERN, iBletchleyPark, iWembley, iGraceland, iCristoRedentor, iItaipuDam, iHollywood, iUnitedNations, iCNTower, iGoldenGateBridge, 
-iAtomium, iGlobalSeedVault, iWorldTradeCenter, iHarbourOpera, iLotusTemple, iBurjKhalifa, iHubbleSpaceTelescope, iChannelTunnel, iDeltaWorks, iIronDome, 
-iOrientalPearlTower, iGardensByTheBay, iSpaceElevator, iLargeHadronCollider, iITER) = range(iNumBuildings)
+iForbiddenPalace, iSpiralMinaret, iDomeOfTheRock, iHouseOfWisdom, iUniversityOfSankore, iTajMahal, iOxfordUniversity, iSanMarcoBasilica, iPorcelainTower, iSaintBasilsCathedral, 
+iEscorial, iTorreDeBelem, iHarmandirSahib, iTrafalgarSquare, iPotalaPalace, iShalimarGardens, iImageOfTheWorldSquare, iBourse, iItsukushimaShrine, iLouvre, 
+iHermitage, iSaltCathedral, iAmberRoom, iEmeraldBuddha, iGuadalupeBasilica, iBellRockLighthouse, iBrandenburgGate, iStatueOfLiberty, iTriumphalArch, iMoleAntonelliana, 
+iMetropolitain, iNeuschwanstein, iBrooklynBridge, iCrystalPalace, iTsukijiFishMarket, iPentagon, iMotherlandCalls, iLubyanka, iWestminsterPalace, iEiffelTower, 
+iEmpireStateBuilding, iPalaceOfNations, iSagradaFamilia, iBerlaymont,iCERN, iBletchleyPark, iWembley, iGraceland, iCristoRedentor, iItaipuDam, 
+iHollywood, iUnitedNations, iCNTower, iGoldenGateBridge, iAtomium, iGlobalSeedVault, iWorldTradeCenter, iHarbourOpera, iLotusTemple, iBurjKhalifa, 
+iHubbleSpaceTelescope, iChannelTunnel, iDeltaWorks, iIronDome, iOrientalPearlTower, iGardensByTheBay, iSpaceElevator, iLargeHadronCollider, iITER) = range(iNumBuildings)
 
 iBeginWonders = iGreatSphinx # different from DLL constant because that includes national wonders
 
--- CvGameCoreDLL/CvCity.cpp
+++ CvGameCoreDLL/CvCity.cpp
@@ -4664,7 +4664,7 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bObsolet
 		if (eBuilding == POTALA_PALACE)
 		{
 			int iNumHills = 0;
-			for (int iI = 0; iI < NUM_CITY_PLOTS; iI++)
+			for (iI = 0; iI < NUM_CITY_PLOTS; iI++)
 			{
 				if (getCityIndexPlot(iI)->isHills())
 				{
@@ -4685,7 +4685,7 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bObsolet
 		if (eBuilding == ITSUKUSHIMA_SHRINE)
 		{
 			int iNumWater = 0;
-			for (int iI = 0; iI < NUM_CITY_PLOTS; iI++)
+			for (iI = 0; iI < NUM_CITY_PLOTS; iI++)
 			{
 				if (getCityIndexPlot(iI)->isWater())
 				{
@@ -4700,7 +4700,7 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bObsolet
 		if (eBuilding == MOLE_ANTONELLIANA)
 		{
 			int iNumPeaks = 0;
-			for (int iI = 0; iI < NUM_CITY_PLOTS; iI++)
+			for (iI = 0; iI < NUM_CITY_PLOTS; iI++)
 			{
 				if (getCityIndexPlot(iI)->isPeak())
 				{
@@ -4711,11 +4711,31 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bObsolet
 			changeBuildingYieldChange((BuildingClassTypes)GC.getBuildingInfo((BuildingTypes)MOLE_ANTONELLIANA).getBuildingClassType(), YIELD_PRODUCTION, iChange * 2 * iNumPeaks);
 		}
 
+		// Metropolitain
+		if (eBuilding == METROPOLITAIN)
+		{
+			for (iI = 0; iI < GC.getNumBuildingInfos(); iI++)
+			{
+				int iNumBuildings = 0;
+				if (isHasRealBuilding((BuildingTypes)iI))
+				{
+					iNumBuildings += 1;
+				}
+					
+				changeBuildingYieldChange((BuildingClassTypes)GC.getBuildingInfo(eBuilding).getBuildingClassType(), YIELD_COMMERCE, iChange * iNumBuildings);
+			}
+		}
+
+		if (isHasBuildingEffect((BuildingTypes)METROPOLITAIN))
+		{
+			changeBuildingYieldChange((BuildingClassTypes)GC.getBuildingInfo((BuildingTypes)METROPOLITAIN).getBuildingClassType(), YIELD_COMMERCE, iChange);
+		}
+
 		// Berlaymont
 		if (eBuilding == BERLAYMONT)
 		{
 			int iNumDefensivePacts = 0;
-			for (int iI = 0; iI < MAX_TEAMS; iI++)
+			for (iI = 0; iI < MAX_TEAMS; iI++)
 			{
 				if (getTeam() != iI && GET_TEAM((TeamTypes)iI).isAlive() && ! GET_TEAM((TeamTypes)iI).isMinorCiv() && GET_TEAM(getTeam()).isDefensivePact((TeamTypes)iI))
 				{
@@ -4729,7 +4749,7 @@ void CvCity::processBuilding(BuildingTypes eBuilding, int iChange, bool bObsolet
 		// Atomium
 		if (eBuilding == ATOMIUM)
 		{
-			for (int iI = 0; iI < MAX_PLAYERS; iI++)
+			for (iI = 0; iI < MAX_PLAYERS; iI++)
 			{
 				changeBuildingCommerceChange((BuildingClassTypes)GC.getBuildingInfo(eBuilding).getBuildingClassType(), COMMERCE_RESEARCH, iChange * GET_PLAYER((PlayerTypes)iI).getNumNukeUnits());
 			}
