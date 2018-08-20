59a5a2b75049ac8546220bccc5d1ec555e36e62f
dguenms <dguenms@gmail.com>
2018-08-20 20:15:21 +0200

    Add Kremlin, adjust other effects

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -872,7 +872,7 @@ iSugar, iTea, iTobacco, iWine, iWhales, iSoccer, iSongs, iMovies) = range(iNumBo
 
 # Buildings
 
-iNumBuildings = 306
+iNumBuildings = 307
 (iPalace, iWalls, iDun, iCastle, iCitadel, iStarFort, iBarracks, iIkhanda, iStable, iGer, 
 iBunker, iBombShelters, iGranary, iTerrace, iSmokehouse, iAqueduct, iBaray, iNoria, iStepwell, iBath, 
 iReservoir, iHammam, iPharmacy, iAlchemist, iSewer, iPark, iHospital, iRecyclingCenter, iLighthouse, iTradingPost, 
@@ -895,15 +895,15 @@ iHangingGardens, iOracle, iMoaiStatues, iIshtarGate, iApadanaPalace, iColossus, 
 iKhajuraho, iGreatLibrary, iMausoleumOfMaussollos, iAlKhazneh, iGondeshapur, iFloatingGardens, iColosseum, iAquaAppia, iIronPillar, iDujiangyan, 
 iGreatWall, iJetavanaramaya, iTheodosianWalls, iMachuPicchu, iSalsalBuddha, iPrambanan, iBorobudur, iMonolithicChurch, iGrandCanal, iHagiaSophia, 
 iNotreDame, iSaintSophia, iTempleOfKukulkan, iHimejiCastle, iAlamut, iOldSynagogue, iKrakDesChevaliers, iSaintThomasChurch, iBlueMosque, iWatPreahPisnulok, 
-iVijayaStambha, iTopkapiPalace, iSilverTreeFountain, iMezquita, iCheomseongdae, iNalanda, iSistineChapel, iSantaMariaDelFiore, iGurEAmir, iRedFort, 
-iVersailles, iForbiddenPalace, iSpiralMinaret, iDomeOfTheRock, iHouseOfWisdom, iUniversityOfSankore, iTajMahal, iOxfordUniversity, iSanMarcoBasilica, iPorcelainTower, 
-iSaintBasilsCathedral, iEscorial, iTorreDeBelem, iHarmandirSahib, iTrafalgarSquare, iPotalaPalace, iShalimarGardens, iImageOfTheWorldSquare, iBourse, iItsukushimaShrine, 
-iLouvre, iHermitage, iSaltCathedral, iAmberRoom, iEmeraldBuddha, iGuadalupeBasilica, iAbbeyMills, iBellRockLighthouse, iBrandenburgGate, iStatueOfLiberty, 
-iTriumphalArch, iMoleAntonelliana, iNobelPrize, iMetropolitain, iNeuschwanstein, iBrooklynBridge, iMenloPark, iCrystalPalace, iTsukijiFishMarket, iPentagon, 
-iMotherlandCalls, iLubyanka, iWestminsterPalace, iEiffelTower, iEmpireStateBuilding, iLasLajasSanctuary, iPalaceOfNations, iSagradaFamilia, iBerlaymont, iCERN, 
-iBletchleyPark, iWembley, iGraceland, iCristoRedentor, iItaipuDam, iHollywood, iUnitedNations, iCNTower, iCrystalCathedral, iGoldenGateBridge, 
-iAtomium, iGlobalSeedVault, iWorldTradeCenter, iHarbourOpera, iLotusTemple, iBurjKhalifa, iHubbleSpaceTelescope, iChannelTunnel, iDeltaWorks, iIronDome, 
-iOrientalPearlTower, iSkytree, iGardensByTheBay, iSpaceElevator, iLargeHadronCollider, iITER) = range(iNumBuildings)
+iKremlin, iVijayaStambha, iTopkapiPalace, iSilverTreeFountain, iMezquita, iCheomseongdae, iNalanda, iSistineChapel, iSantaMariaDelFiore, iGurEAmir, 
+iRedFort, iVersailles, iForbiddenPalace, iSpiralMinaret, iDomeOfTheRock, iHouseOfWisdom, iUniversityOfSankore, iTajMahal, iOxfordUniversity, iSanMarcoBasilica, 
+iPorcelainTower, iSaintBasilsCathedral, iEscorial, iTorreDeBelem, iHarmandirSahib, iTrafalgarSquare, iPotalaPalace, iShalimarGardens, iImageOfTheWorldSquare, iBourse, 
+iItsukushimaShrine, iLouvre, iHermitage, iSaltCathedral, iAmberRoom, iEmeraldBuddha, iGuadalupeBasilica, iAbbeyMills, iBellRockLighthouse, iBrandenburgGate, 
+iStatueOfLiberty, iTriumphalArch, iMoleAntonelliana, iNobelPrize, iMetropolitain, iNeuschwanstein, iBrooklynBridge, iMenloPark, iCrystalPalace, iTsukijiFishMarket, 
+iPentagon, iMotherlandCalls, iLubyanka, iWestminsterPalace, iEiffelTower, iEmpireStateBuilding, iLasLajasSanctuary, iPalaceOfNations, iSagradaFamilia, iBerlaymont, 
+iCERN, iBletchleyPark, iWembley, iGraceland, iCristoRedentor, iItaipuDam, iHollywood, iUnitedNations, iCNTower, iCrystalCathedral, 
+iGoldenGateBridge, iAtomium, iGlobalSeedVault, iWorldTradeCenter, iHarbourOpera, iLotusTemple, iBurjKhalifa, iHubbleSpaceTelescope, iChannelTunnel, iDeltaWorks, 
+iIronDome, iOrientalPearlTower, iSkytree, iGardensByTheBay, iSpaceElevator, iLargeHadronCollider, iITER) = range(iNumBuildings)
 
 iBeginWonders = iGreatSphinx # different from DLL constant because that includes national wonders
 
