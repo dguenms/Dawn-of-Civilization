d0bd1916840aadcf1b97e86fcdaa08811f9cce47
dguenms <dguenms@gmail.com>
2018-09-09 17:32:30 +0200

    Add Fiber Network

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -871,7 +871,7 @@ iDeer, iFish, iPig, iRice, iSheep, iWheat, iCoffee, iCotton, iDye, iFur, iGems, 
 iSugar, iTea, iTobacco, iWine, iWhales, iSoccer, iSongs, iMovies) = range(iNumBonuses)
 # Buildings
 
-iNumBuildings = 314
+iNumBuildings = 315
 (iPalace, iBarracks, iIkhanda, iGranary, iTerrace, iSmokehouse, iPaganTemple, iMonument, iObelisk, iStele,
 iCandi, iEdict, iMalae, iTotemPole, iWalls, iDun, iStable, iGer, iLibrary, iEdubba,
 iTaixue, iHoTrai, iSangam, iHarbor, iAqueduct, iBaray, iNoria, iStepwell, iTheatre, iOdeon,
@@ -883,7 +883,8 @@ iSeowon, iGompa, iCivicSquare, iRathaus, iSejmik, iSewer, iStarFort, iEstate, iM
 iHacienda, iDrydock, iLevee, iDike, iObservatory, iWarehouse, iCourthouse, iFactory, iAssemblyPlant, iZaibatsu, 
 iDistillery, iPark, iCoalPlant, iRailwayStation, iLaboratory, iResearchInstitute, iNewsPress, iIndustrialPark, iCinema, iHospital, 
 iSupermarket, iColdStoragePlant, iPublicTransportation, iDepartmentStore, iMall, iBroadcastTower, iIntelligenceAgency, iElectricalGrid, iAirport, iBombShelters, 
-iHydroPlant, iBunker, iNuclearPlant, iSecurityBureau, iHotel, iRecyclingCenter, iContainerTerminal, iLogisticsCenter, iSolarPlant, iAutomatedFactory, 
+iHydroPlant, iBunker, iNuclearPlant, iSecurityBureau, iHotel, iRecyclingCenter, iContainerTerminal, iLogisticsCenter, iSolarPlant, iFiberNetwork,
+iAutomatedFactory, 
 iVerticalFarm,
 iJewishTemple, 
 iJewishCathedral, 
