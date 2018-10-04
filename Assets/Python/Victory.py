e3106a22010ea28f7b35f6a7acb62f1e6565e614
dguenms <dguenms@gmail.com>
2018-10-05 01:45:36 +0200

    1700 AD Turks start with all UHV goals expiredd

--- Assets/Python/Victory.py
+++ Assets/Python/Victory.py
@@ -231,7 +231,7 @@ def setup():
 
 	# 1700 AD scenario: handle dates that have already been passed
 	if utils.getScenario() == i1700AD:
-		for iPlayer in [iChina, iIndia, iTamils, iKorea, iVikings, iSpain, iHolyRome, iPoland, iPortugal, iMughals, iOttomans, iThailand]:
+		for iPlayer in [iChina, iIndia, iTamils, iKorea, iVikings, iTurks, iSpain, iHolyRome, iPoland, iPortugal, iMughals, iOttomans, iThailand]:
 			loseAll(iPlayer)
 			
 		win(iPersia, 0)
