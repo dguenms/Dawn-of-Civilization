071b610f18574d56dd861f69f11bada402e47b13
dguenms <dguenms@gmail.com>
2018-10-23 00:29:09 +0200

    Italian unique units: Balestriere and Lanternas

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -829,27 +829,27 @@ iTranshumanism) = range(iNumTechs)
 
 # initialise unit variables to unit indices from XML
 
-iNumUnits = 197
+iNumUnits = 198
 (iLion, iBear, iPanther, iWolf, iSettler, iCityBuilder, iWorker, iAmericanPioneer, iPunjabiWorker, iLabourer, 
 iMadeireiro, iScout, iExplorer, iSpy, iReligiousPersecutor, iJewishMissionary, iOrthodoxMissionary, iCatholicMissionary, iProtestantMissionary, iIslamicMissionary, 
 iHinduMissionary, iBuddhistMissionary, iConfucianMissionary, iTaoistMissionary, iZoroastrianMissionary, iWarrior, iMilitia, iAxeman, iLightSwordsman, iVulture, 
 iDogSoldier, iSwordsman, iJaguar, iLegion, iGallicWarrior, iAucac, iShotelai, iHeavySwordsman, iSamurai, iHuscarl, 
 iGhazi, iPombos, iSpearman, iHoplite, iSacredBand, iImmortal, iImpi, iHeavySpearman, iPikeman, iLandsknecht, 
 iArquebusier, iTercio, iJanissary, iOromoWarrior, iQizilbash, iMohawk, iMusketeer, iMusketman, iRedcoat, iMinuteman, 
-iRifleman, iGrenadier, iGrenzer, iAlbionLegion, iAntiTank, iInfantry, iBersagliere, iSamInfantry, iMobileSam, iMarine, 
-iNavySeal, iParatrooper, iMechanizedInfantry, iArcher, iAsharittuBowman, iMedjay, iSkirmisher, iHolkan, iKelebolo, iLongbowman, 
-iPatiyodha, iCrossbowman, iChokonu, iChariot, iWarChariot, iHuluganni, iCidainh, iHorseman, iCompanion, iNumidianCavalry, 
+iRifleman, iGrenadier, iGrenzer, iAlbionLegion, iAntiTank, iInfantry, iSamInfantry, iMobileSam, iMarine, iNavySeal, 
+iParatrooper, iMechanizedInfantry, iArcher, iAsharittuBowman, iMedjay, iSkirmisher, iHolkan, iKelebolo, iLongbowman, iPatiyodha, 
+iCrossbowman, iChokonu, iBalestriere, iChariot, iWarChariot, iHuluganni, iCidainh, iHorseman, iCompanion, iNumidianCavalry, 
 iAsvaka, iCamelRider, iHorseArcher, iKhampa, iOghuz, iCamelArcher, iLancer, iMobileGuard, iKeshik, iCataphract, 
 iChangSuek, iGhulamWarrior, iFarari, iPistolier, iCamelGunner, iCuirassier, iGendarme, iConquistador, iWingedHussar, iMountedBrave, 
 iCamelGunner, iHussar, iDragoon, iGuard, iCossack, iGrenadierCavalry, iCavalry, iRurales, iWarElephant, iBallistaElephant, 
 iAtlasElephant, iTank, iPanzer, iMainBattleTank, iGunship, iCatapult, iBallista, iHwacha, iTrebuchet, iBombard, 
 iSiegeElephant, iCannon, iArtillery, iMachineGun, iHowitzer, iMobileArtillery, iWorkboat, iGalley, iWaka, iBireme, 
-iWarGalley, iHeavyGalley, iDromon, iLongship, iDharani, iCog, iGalleass, iKobukson, iCaravel, iCarrack, 
-iGalleon, iEastIndiaman, iPrivateer, iOrangLaut, iCorsair, iFrigate, iShipOfTheLine, iManOfWar, iIronclad, iTorpedoBoat, 
-iCruiser, iTransport, iDestroyer, iCorvette, iBattleship, iMissileCruiser, iStealthDestroyer, iSubmarine, iNuclearSubmarine, iCarrier, 
-iBiplane, iFighter, iZero, iJetFighter, iBomber, iStealthBomber, iGuidedMissile, iNuclearBomber, iICBM, iGreatProphet, 
-iGreatArtist, iGreatScientist, iGreatMerchant, iGreatEngineer, iGreatStatesman, iGreatGeneral, iGreatSpy, iFemaleGreatProphet, iFemaleGreatArtist, iFemaleGreatScientist, 
-iFemaleGreatMerchant, iFemaleGreatEngineer, iFemaleGreatStatesman, iFemaleGreatGeneral, iFemaleGreatSpy, iSlave, iAztecSlave) = range(iNumUnits)
+iWarGalley, iHeavyGalley, iDromon, iLongship, iDharani, iCog, iGalleass, iKobukson, iLanternas, iCaravel, 
+iCarrack, iGalleon, iEastIndiaman, iPrivateer, iOrangLaut, iCorsair, iFrigate, iShipOfTheLine, iManOfWar, iIronclad, 
+iTorpedoBoat, iCruiser, iTransport, iDestroyer, iCorvette, iBattleship, iMissileCruiser, iStealthDestroyer, iSubmarine, iNuclearSubmarine, 
+iCarrier, iBiplane, iFighter, iZero, iJetFighter, iBomber, iStealthBomber, iGuidedMissile, iNuclearBomber, iICBM, 
+iGreatProphet, iGreatArtist, iGreatScientist, iGreatMerchant, iGreatEngineer, iGreatStatesman, iGreatGeneral, iGreatSpy, iFemaleGreatProphet, iFemaleGreatArtist, 
+iFemaleGreatScientist, iFemaleGreatMerchant, iFemaleGreatEngineer, iFemaleGreatStatesman, iFemaleGreatGeneral, iFemaleGreatSpy, iSlave, iAztecSlave) = range(iNumUnits)
 
 iMissionary = iJewishMissionary # generic
 
