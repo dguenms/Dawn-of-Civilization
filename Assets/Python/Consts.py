e0ad36b1ddbfdd64d9d0cff29e8efa1b2f739dee
dguenms <dguenms@gmail.com>
2018-10-22 22:21:43 +0200

    Viking unique unit: Longship

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -829,7 +829,7 @@ iTranshumanism) = range(iNumTechs)
 
 # initialise unit variables to unit indices from XML
 
-iNumUnits = 195
+iNumUnits = 196
 (iLion, iBear, iPanther, iWolf, iSettler, iCityBuilder, iWorker, iAmericanPioneer, iPunjabiWorker, iLabourer, 
 iMadeireiro, iScout, iExplorer, iSpy, iReligiousPersecutor, iJewishMissionary, iOrthodoxMissionary, iCatholicMissionary, iProtestantMissionary, iIslamicMissionary, 
 iHinduMissionary, iBuddhistMissionary, iConfucianMissionary, iTaoistMissionary, iZoroastrianMissionary, iWarrior, iMilitia, iAxeman, iLightSwordsman, iVulture, 
@@ -844,12 +844,12 @@ iChangSuek, iGhulamWarrior, iFarari, iPistolier, iCamelGunner, iCuirassier, iGen
 iCamelGunner, iHussar, iDragoon, iGuard, iCossack, iGrenadierCavalry, iCavalry, iRurales, iWarElephant, iBallistaElephant, 
 iAtlasElephant, iTank, iPanzer, iMainBattleTank, iGunship, iCatapult, iBallista, iHwacha, iTrebuchet, iBombard, 
 iSiegeElephant, iCannon, iArtillery, iMachineGun, iHowitzer, iMobileArtillery, iWorkboat, iGalley, iWaka, iBireme, 
-iWarGalley, iHeavyGalley, iDharani, iCog, iGalleass, iKobukson, iCaravel, iCarrack, iGalleon, iEastIndiaman, 
-iPrivateer, iOrangLaut, iCorsair, iFrigate, iShipOfTheLine, iManOfWar, iIronclad, iTorpedoBoat, iCruiser, iTransport, 
-iDestroyer, iCorvette, iBattleship, iMissileCruiser, iStealthDestroyer, iSubmarine, iNuclearSubmarine, iCarrier, iBiplane, iFighter, 
-iZero, iJetFighter, iBomber, iStealthBomber, iGuidedMissile, iNuclearBomber, iICBM, iGreatProphet, iGreatArtist, iGreatScientist, 
-iGreatMerchant, iGreatEngineer, iGreatStatesman, iGreatGeneral, iGreatSpy, iFemaleGreatProphet, iFemaleGreatArtist, iFemaleGreatScientist, iFemaleGreatMerchant, iFemaleGreatEngineer, 
-iFemaleGreatStatesman, iFemaleGreatGeneral, iFemaleGreatSpy, iSlave, iAztecSlave) = range(iNumUnits)
+iWarGalley, iHeavyGalley, iLongship, iDharani, iCog, iGalleass, iKobukson, iCaravel, iCarrack, iGalleon, 
+iEastIndiaman, iPrivateer, iOrangLaut, iCorsair, iFrigate, iShipOfTheLine, iManOfWar, iIronclad, iTorpedoBoat, iCruiser, 
+iTransport, iDestroyer, iCorvette, iBattleship, iMissileCruiser, iStealthDestroyer, iSubmarine, iNuclearSubmarine, iCarrier, iBiplane, 
+iFighter, iZero, iJetFighter, iBomber, iStealthBomber, iGuidedMissile, iNuclearBomber, iICBM, iGreatProphet, iGreatArtist, 
+iGreatScientist, iGreatMerchant, iGreatEngineer, iGreatStatesman, iGreatGeneral, iGreatSpy, iFemaleGreatProphet, iFemaleGreatArtist, iFemaleGreatScientist, iFemaleGreatMerchant, 
+iFemaleGreatEngineer, iFemaleGreatStatesman, iFemaleGreatGeneral, iFemaleGreatSpy, iSlave, iAztecSlave) = range(iNumUnits)
 
 iMissionary = iJewishMissionary # generic
 
