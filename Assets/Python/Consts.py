0161996aa69bf755c902c782a93eaa44e645844c
dguenms <dguenms@gmail.com>
2019-01-07 22:53:01 +0100

    Korean Hwacha replaces Bombard

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -843,7 +843,7 @@ iHuluganni, iCidainh, iHorseman, iCompanion, iNumidianCavalry, iAsvaka, iCamelRi
 iOghuz, iCamelArcher, iLancer, iVaru, iSavaran, iMobileGuard, iKeshik, iCataphract, iChangSuek, iGhulamWarrior, 
 iFarari, iPistolier, iCamelGunner, iCuirassier, iGendarme, iConquistador, iWingedHussar, iMountedBrave, iHussar, iLlanero, 
 iDragoon, iGuard, iCossack, iGrenadierCavalry, iCavalry, iRural, iWarElephant, iBallistaElephant, iTank, iPanzer, 
-iMainBattleTank, iGunship, iCatapult, iBallista, iHwacha, iTrebuchet, iBombard, iSiegeElephant, iGreatBombard, iCannon, 
+iMainBattleTank, iGunship, iCatapult, iBallista, iTrebuchet, iBombard, iHwacha, iSiegeElephant, iGreatBombard, iCannon, 
 iArtillery, iMachineGun, iHowitzer, iMobileArtillery, iWorkboat, iGalley, iWaka, iBireme, iWarGalley, iHeavyGalley, 
 iDromon, iLongship, iDharani, iCog, iGalleass, iDjong, iKobukson, iLanternas, iCaravel, iCarrack, 
 iGalleon, iEastIndiaman, iPrivateer, iCorsair, iFrigate, iShipOfTheLine, iManOfWar, iIronclad, iTorpedoBoat, iCruiser, 
