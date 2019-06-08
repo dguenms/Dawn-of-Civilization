5db0186b2cd3a2b7877c5106fb1aa88c158a0b42
dguenms <dguenms@gmail.com>
2019-06-08 21:21:53 +0200

    Flood plains and oasis makes desert passable

--- CvGameCoreDLL/CvInfos.cpp
+++ CvGameCoreDLL/CvInfos.cpp
@@ -14400,6 +14400,7 @@ m_bNoCity(false),
 m_bNoImprovement(false),
 m_bVisibleAlways(false),
 m_bNukeImmune(false),
+m_bMakesPassable(false), // Leoreth
 m_iWorldSoundscapeScriptId(0),
 m_iEffectProbability(0),
 m_piYieldChange(NULL),
@@ -14518,6 +14519,12 @@ bool CvFeatureInfo::isAddsFreshWater() const
 bool CvFeatureInfo::isImpassable() const
 {
 	return m_bImpassable;
+}
+
+// Leoreth
+bool CvFeatureInfo::isMakesPassable() const
+{
+	return m_bMakesPassable;
 }
 
 bool CvFeatureInfo::isNoCity() const
@@ -14698,6 +14705,7 @@ bool CvFeatureInfo::read(CvXMLLoadUtility* pXML)
 	pXML->GetChildXmlValByName(&m_bNoImprovement, "bNoImprovement");
 	pXML->GetChildXmlValByName(&m_bVisibleAlways, "bVisibleAlways");
 	pXML->GetChildXmlValByName(&m_bNukeImmune, "bNukeImmune");
+	pXML->GetChildXmlValByName(&m_bMakesPassable, "bMakesPassable");
 	pXML->GetChildXmlValByName(m_szOnUnitChangeTo, "OnUnitChangeTo");
 
 	pXML->SetVariableListTagPairForAudioScripts(&m_pi3DAudioScriptFootstepIndex, "FootstepSounds", GC.getFootstepAudioTypes(), GC.getNumFootstepAudioTypes());
