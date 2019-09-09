d8cc07758711380d537f9cac7b67f9ebaaf65a4a
dguenms <dguenms@gmail.com>
2019-09-09 02:15:03 +0200

    Edit city name map via map tags

--- Assets/Python/MapUtils.py
+++ Assets/Python/MapUtils.py
@@ -4,8 +4,6 @@ from RFCUtils import utils
 
 import csv
 
-BASE_PATH = 'D:\DoC Maps\%s.csv'
-
 def convertMap(value):
 	rows = []
 
@@ -17,17 +15,54 @@ def convertMap(value):
 		rows.append(row)
 		
 	return rows
-
-def printMap(rows, name):
-	file = open(BASE_PATH % name, 'wb')
 	
-	try:
-		writer = csv.writer(file)
-		for row in rows:
-			writer.writerow(row)
-	finally:
-		file.close()
+def retrieveSigns():
+	dSigns = {}
+	
+	engine = CyEngine()
+	for iSign in range(engine.getNumSigns()):
+		sign = engine.getSignByIndex(iSign)
+		plot = sign.getPlot()
+		iPlayer = sign.getPlayerType()
+		caption = sign.getCaption()
 		
+		if iPlayer == -1:
+			x, y = plot.getX(), plot.getY()
+			dSigns[(x, y)] = caption.encode('latin-1')
+			
+	def sign(plot):
+		x, y = plot.getX(), plot.getY()
+		if (x, y) in dSigns: return dSigns[(x, y)]
+		return ''
+		
+	return convertMap(sign)
+	
+def placeSigns(rows):
+	engine = CyEngine()
+	for y in range(len(rows)):
+		row = rows[y]
+		for x in range(len(row)):
+			caption = row[x]
+			if caption:
+				engine.addLandmark(gc.getMap().plot(x, iWorldY-1-y), caption)
+
+def saveCityNames():
+	current = utils.readMap('Cities')
+	updated = retrieveSigns()
+	
+	utils.writeMap(updated, 'CitiesDiff')
+	
+	for y in range(min(iWorldY, len(current))):
+		row = current[y]
+		for x in range(min(iWorldX, len(row))):
+			if not updated[y][x]:
+				updated[y][x] = row[x]
+	
+	utils.writeMap(updated, 'Cities')
+	
+def loadCityNames():
+	rows = utils.readMap('Cities')
+	placeSigns(rows)
 
 def landOrWater(plot):
 	if plot.isWater() or plot.isPeak():
