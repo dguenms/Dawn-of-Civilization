39586f0ecfbbd3952f6b403815a467415a2602bd
MeuhV1 <merijn_v1@outlook.com>
2019-11-06 14:56:14 +0100

    Added Emerald bonus variety - changed default gems in Colombia to emeralds - changed diamond variety in Zambia to emeralds - changed default gems near Brazil to diamonds

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -874,9 +874,9 @@ iGems, iGold, iIncense, iIvory, iJade, iMillet, iObsidian, iOlives, iOpium, iPea
 iRareEarths, iRubber, iSalt, iSilk, iSilver, iSpices, iSugar, iTea, iTobacco, iWine,
 iWhales, iSoccer, iSongs, iMovies) = range(iNumBonuses)
 
-iNumBonusVarieties = 15
-(iDyeMurex, iDyeHenna, iSpicesNutmeg, iSpicesSaffron, iSpicesVanilla, iGemsTurquoise, iGemsDiamonds, iGemsRuby, iGemsSapphire, iSheepLlama,
-iSheepBlack, iCowBrown, iPigFurry, iIvoryAfrican, iCitrusOranges) = range(iNumBonuses, iNumBonuses + iNumBonusVarieties)
+iNumBonusVarieties = 16
+(iDyeMurex, iDyeHenna, iSpicesNutmeg, iSpicesSaffron, iSpicesVanilla, iGemsTurquoise, iGemsDiamonds, iGemsRuby, iGemsSapphire, iGemsEmeralds,
+iSheepLlama, iSheepBlack, iCowBrown, iPigFurry, iIvoryAfrican, iCitrusOranges) = range(iNumBonuses, iNumBonuses + iNumBonusVarieties)
 
 # Buildings
 
