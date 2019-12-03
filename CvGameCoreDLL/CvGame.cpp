a81d5d644ead7cee5bd95830edaddf232f68017e
dguenms <dguenms@gmail.com>
2019-12-03 23:49:57 +0100

    Players can still collapse after winning the game

--- CvGameCoreDLL/CvGame.cpp
+++ CvGameCoreDLL/CvGame.cpp
@@ -2310,7 +2310,7 @@ void CvGame::update()
 
 		testAlive();
 
-		if ((getAIAutoPlay() == 0) && !(gDLL->GetAutorun()) && GAMESTATE_EXTENDED != getGameState())
+		if ((getAIAutoPlay() == 0) && !(gDLL->GetAutorun()) /*&& GAMESTATE_EXTENDED != getGameState()*/)
 		{
 			if (countHumanPlayersAlive() == 0)
 			{
