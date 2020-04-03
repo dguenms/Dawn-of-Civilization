c70a51992dfef3052ce3728c5ac6f8d528323762
earthcrafterman <banddstudios@gmail.com>
2020-04-03 15:54:14 -0400

    Fixed an issue where the Yemen Arab Republic Dynamic Name is used long before it should be. I have no clue how it took this long for me to notice this issue.

--- Assets/Python/DynamicCivs.py
+++ Assets/Python/DynamicCivs.py
@@ -2149,12 +2149,12 @@ def specificTitle(iPlayer, lPreviousOwners=[]):
 			return "TXT_KEY_CIV_YEMEN_MECCA"
 		if iCivicReligion == iTheocracy:
 			return "TXT_KEY_CIV_YEMEN_THEOCRACY"
-		if iCivicGovernment == iDespotism or iCivicGovernment == iStateParty:
+		if not pArabia.isAlive():
+			return "TXT_KEY_CIV_YEMEN_DEAD_ARABIA"
+		if (iCivicGovernment == iDespotism and iEra >= iGlobal) or iCivicGovernment == iStateParty:
 			return "TXT_KEY_CIV_YEMEN_STATE_PARTY"
 		if iCivicGovernment == iElective:
 			return "TXT_KEY_CIV_YEMEN_ELECTIVE"
-		if not pArabia.isAlive():
-			return "TXT_KEY_CIV_YEMEN_DEAD_ARABIA"
 		return "TXT_KEY_CIV_YEMEN_DEFAULT"
 		
 	elif iPlayer == iIndonesia:
