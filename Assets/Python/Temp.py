1b98fe56b81907f7aa491614a09b7f59c563254c
dguenms <dguenms@gmail.com>
2021-07-24 16:04:05 +0200

    Remove unneeded files

--- Assets/Python/Temp.py
+++
@@ -1,9 +0,0 @@
-from Consts import *
-from RFCUtils import utils
-from Stability import doResurrection
-
-
-TL = (99, 43)
-BR = (104, 48)
-
-doResurrection(slot(iChina), utils.getAreaCities(utils.getPlotList(TL, BR)))
--- Assets/Python/Test.py
+++
@@ -1,8 +0,0 @@
-def function():
-	print "you have called the function"
-
-name = 'name'
-othername = 'othername'
-
-globals()[name] = function
-globals()[othername] = function
