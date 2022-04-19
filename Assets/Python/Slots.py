b467cd66c0ed430fd528e1679cbfaf5c70b02cc0
dguenms <dguenms@gmail.com>
2022-04-20 00:31:08 +0200

    Stability is reset when a new civilization is assigned to a player

--- Assets/Python/Slots.py
+++ Assets/Python/Slots.py
@@ -38,6 +38,8 @@ def updateCivilization(iPlayer, iCiv):
 	
 	if iCurrentCivilization in data.dSlots:
 		del data.dSlots[iCurrentCivilization]
+		
+	data.players[iPlayer].resetStability()
 
 def getImpact(iCiv):
 	iActiveCiv = civ()
