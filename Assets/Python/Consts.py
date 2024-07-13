fec008c1b0419af0465d7a20a1c23d8420d352a2
dguenms <dguenms@gmail.com>
2024-07-13 21:22:35 +0200

    Mongols target the Rus instead of Russia

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -1166,7 +1166,7 @@ iNumImpacts = 5
 lTradingCompanyCivs = [iSpain, iFrance, iEngland, iPortugal, iNetherlands]
 lLateColonyCivs = lTradingCompanyCivs + [iGermany]
 
-lMongolCivs = [iPersia, iByzantium, iTurks, iArabia, iRussia]
+lMongolCivs = [iPersia, iByzantium, iTurks, iArabia, iRus]
 
 (i3000BC, i600AD, i1700AD) = range(3)
 
