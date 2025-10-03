31880bc61f0397b505c6bd1ec94dcf05876d1e9e
dguenms <dguenms@gmail.com>
2025-10-03 16:27:52 +0200

    Korean Seowon: requires Education, +1 scientist slot

--- Assets/Python/HistoricalVictory.py
+++ Assets/Python/HistoricalVictory.py
@@ -391,7 +391,7 @@ dGoals = {
 	),
 	iKorea: (
 		BuildingCount((iBuddhistCathedral, 1), (iConfucianCathedral, 1), by=1000),
-		FirstDiscover(iEducation, Printing, iFirearms, iStatecraft),
+		FirstDiscover(iEducation, iPrinting, iFirearms, iStatecraft),
 		SunkShips(20),
 	),
 	iKhmer: (
