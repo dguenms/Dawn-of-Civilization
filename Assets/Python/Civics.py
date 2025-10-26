349b90bfd8e6c2203e3e2932753aebc79e0e7f87
dguenms <dguenms@gmail.com>
2025-10-26 20:29:47 +0100

    Correct city states name condition

--- Assets/Python/Civics.py
+++ Assets/Python/Civics.py
@@ -107,9 +107,6 @@ def isCityStates(iPlayer):
 	if civic.iGovernment in [iRepublic, iDemocracy] and civic.iLegitimacy in [iPersonalism, iCitizenship, iBureaucracy]:
 		return True
 	
-	if civic.iGovernment == iElective and civic.iLegitimacy in [iCitizenship, iBureaucracy]:
-		return True
-	
 	if civic.iGovernment == iChiefdom and civ(iPlayer) in lCityStatesStart:
 		return True
 	
