a5a6a340844f9d7ef152a10a7f1c04737a0b397c
dguenms <dguenms@gmail.com>
2025-11-19 00:17:34 +0100

    Limit expanded Byzantine flip area

--- Assets/Python/Rise.py
+++ Assets/Python/Rise.py
@@ -524,12 +524,12 @@ class Birth(object):
 	def updateArea(self):
 		if self.iCiv in lExpandedFlipCivs:
 			owners = self.area.cities().owners().major()
-			ownerCities = cities.all().area(self.location).where(lambda city: city.getOwner() in owners).where(lambda city: not plot(city).isPlayerCore(city.getOwner()))
+			ownerCities = cities.all().area(self.location).where(lambda city: city.getOwner() in owners).where(lambda city: not plot(city).isPlayerCore(city.getOwner())).where(lambda city: plot(city).getSettlerValue(self.iCiv) > 0)
 			closerCities = ownerCities.where(lambda city: real_distance(city, self.location) <= real_distance(city, capital(city)))
 			
-			additionalPlots = closerCities.plots().expand(2).where(lambda p: p.getOwner() in owners and none(p.isPlayerCore(iPlayer) for iPlayer in players.major().existing().without(self.iPlayer)))
+			additionalPlots = closerCities.plots().expand(1) + closerCities.plots().expand(2).where(lambda p: p.getSettlerValue(self.iCiv) > 0)
 			
-			self.area += additionalPlots
+			self.area += additionalPlots.where(lambda p: p.getOwner() in owners and none(p.isPlayerCore(iPlayer) for iPlayer in players.major().existing().without(self.iPlayer)))
 			self.area = self.area.unique()
 		
 		if self.iCiv == iMexico:
