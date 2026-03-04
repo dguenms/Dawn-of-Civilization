1d32ee866dd14b11d53604231d9da41cc72baaba
dguenms <dguenms@gmail.com>
2026-03-04 23:50:55 +0100

    Fix bug causing Andean passes to open early in the game

--- Assets/Python/Resources.py
+++ Assets/Python/Resources.py
@@ -432,10 +432,11 @@ def changeConquerorPlotTypes(iConquerorPlayer, iTargetPlayer):
 
 @handler("BeginGameTurn")
 def changeConquerorPlotTypesLate(iGameTurn):
-	for iCiv in lBioNewWorld:
-		if not data.dFirstContactConquerors[iCiv]:
-			for tile, type in dConquerorPlotTypes[iCiv]:
-				plot(tile).setPlotType(type, True, True)
+	if iGameTurn == year(1650):
+		for iCiv in lBioNewWorld:
+			if not data.dFirstContactConquerors[iCiv]:
+				for tile, type in dConquerorPlotTypes[iCiv]:
+					plot(tile).setPlotType(type, True, True)
 
 
 @handler("cityAcquired")
