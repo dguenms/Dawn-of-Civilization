62120a279a22ec211116651dd5834f86d1b40a50
dguenms <dguenms@gmail.com>
2026-03-05 00:34:12 +0100

    Fix early appearance of new world conquerors

--- Assets/Python/History.py
+++ Assets/Python/History.py
@@ -243,17 +243,15 @@ def nativeConquerors(iGameTurn, iPlayer):
 		return
 	
 	if year() >= year(dBirth[iAztecs]):
-		if none(data.dFirstContactConquerors[iCiv] for iCiv in (iMaya, iToltecs, iAztecs)):
+		if none(data.dFirstContactConquerors[iCiv] or player(iCiv).isExisting() for iCiv in (iMaya, iToltecs, iAztecs)):
 			if checkNativeConquerors(iPlayer, tMesoamericanContactZone):
 				for iCiv in (iMaya, iToltecs, iAztecs):
 					data.dFirstContactConquerors[iCiv] = True
-					show("First contact conquerors against: %s", infos.civ(iCiv).getShortDescription(0))
 	
 	if year() >= year(dBirth[iInca]):
-		if not data.dFirstContactConquerors[iInca]:
+		if not data.dFirstContactConquerors[iInca] and not players(iInca).isExisting():
 			if checkNativeConquerors(iPlayer, tAndeanContactZone):
 				data.dFirstContactConquerors[iInca] = True
-				show("First contact conquerors against: Inca")
 	
 
 def checkNativeConquerors(iPlayer, tContactZone):
@@ -295,7 +293,6 @@ def firstContactConquistadors(iTeamX, iHasMetTeamY):
 						tContactZone = tAndeanContactZone
 
 					data.dFirstContactConquerors[iNewWorldCiv] = True
-					show("First contact conquerors against: %s", infos.civ(iNewWorldCiv).getShortDescription(0))
 					
 					if iNewWorldCiv in [iToltecs, iAztecs]:
 						data.dFirstContactConquerors[iToltecs] = True
