3c31a51bcac50a8cd316b2474aad4b098a9f2d71
dguenms <dguenms@gmail.com>
2026-05-03 23:22:54 +0200

    Prevent AI from disbanding units of minor civilizations

--- CvGameCoreDLL/CvPlayerAI.cpp
+++ CvGameCoreDLL/CvPlayerAI.cpp
@@ -515,7 +515,7 @@ void CvPlayerAI::AI_doTurnUnitsPost()
 				bool bKilled = false;
 				if (!bNoDisband)
 				{
-					if (pLoopUnit->canFight() && !pLoopUnit->isFound()) // Leoreth: do not disband American pioneers
+					if (pLoopUnit->canFight() && !pLoopUnit->isFound() && !isMinorCiv()) // Leoreth: do not disband American pioneers or minor units
 					{
 						int iExp = pLoopUnit->getExperience();
 						CvCity* pPlotCity = pLoopUnit->plot()->getPlotCity();
