b20c0dec2d5e199b792d9cd266cafd5ca5827e4d
dguenms <dguenms@gmail.com>
2026-05-03 17:14:01 +0200

    Partial collapse retains respawn area if larger than core area

--- Assets/Python/Collapse.py
+++ Assets/Python/Collapse.py
@@ -108,17 +108,27 @@ def downgradeImprovements(iPlayer):
 			plot.setImprovementType(-1)
 		
 	message(iPlayer, 'TXT_KEY_STABILITY_DOWNGRADE_IMPROVEMENTS', color=iRed)
+	
+def getPartialCollapseCities(iPlayer):
+	respawnCities = cities.respawn(iPlayer).owner(iPlayer)
+	coreCities = cities.core(iPlayer).owner(iPlayer)
+	
+	if respawnCities.count() > coreCities.count():
+		return respawnCities
+	
+	return coreCities
 		
 def collapseToCore(iPlayer):
-	nonCoreCities = cities.owner(iPlayer).where(lambda city: not city.isPlayerCore(iPlayer))
-	ahistoricalCities = nonCoreCities.where(lambda city: plot(city).getPlayerSettlerValue(iPlayer) == 0)
+	retainedCities = getPartialCollapseCities(iPlayer)
+	secededCities = cities.owner(iPlayer).without(retainedCities)
+	ahistoricalCities = secededCities.where(lambda city: plot_(city).getPlayerSettlerValue(iPlayer) == 0)
 	
 	# release all vassals
 	for iVassal in players.vassals(iPlayer):
 		team(iVassal).setVassal(player(iPlayer).getTeam(), False, False)
 				
 	# more than half ahistorical, only secede ahistorical cities
-	if 2 * ahistoricalCities.count() > nonCoreCities.count():
+	if 2 * ahistoricalCities.count() > secededCities.count():
 	
 		# notify owner
 		message(iPlayer, 'TXT_KEY_STABILITY_FOREIGN_SECESSION', color=iRed)
