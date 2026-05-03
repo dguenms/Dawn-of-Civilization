2921e9399f11ee8a7b8bbf0fac71dfd8b300795c
dguenms <dguenms@gmail.com>
2026-05-03 17:16:57 +0200

    Reduce number of defenders granted by Congress city flips

--- Assets/Python/Congresses.py
+++ Assets/Python/Congresses.py
@@ -682,7 +682,7 @@ class Congress:
 		
 		bLimitedDefenders = player(iPlayer).isHuman() or isIsland(flipped)
 		iNumDefenders = bLimitedDefenders and 2 or max(2, player(iPlayer).getCurrentEra()-1)
-		createRoleUnit(iPlayer, flipped, iDefend, iNumDefenders)
+		ensureDefenders(iPlayer, flipped, iNumDefenders)
 		
 	def foundColony(self, iPlayer, (x, y)):
 		plot = plot_(x, y)
